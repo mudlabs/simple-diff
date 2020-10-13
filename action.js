78612c80be9e904472ea92c4f5321510865f3ffc
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-10-13 19:49:56 +1100

    Create action.js

---
+++ action.js
@@ -0,0 +1,42 @@
+const core = require("@actions/core");
+const github = require("@actions/github);
+
+const unsuportedEvent = name => name !== "pull_request" && name !== "push" ? true : false;
+const getBase = name = data => name === "pull_request" ? data.pull_request.base.sha : data.before;
+const getHead = name = data => name === "pull_request" ? data.pull_request.head.sha : data.after;
+
+(async function(){
+  try {
+    const path = core.getInput("path");
+    const token = core.getInput("token");
+    const repo = github.context.repo.repo;
+    const owner = github.context.repo.owner;
+    const event_name = github.context.eventName;
+    const octokit = github.getOctokit(token, { required: true });
+    
+    if (unsupportedEvent(event_name)) throw `This event (${event_name}) is unsupported. Kiss Diff only supports PUSH and PR events.`;
+  
+    const base = getBase(event_name)(github.context.payload);
+    const head = getHead(event_name)(github.context.payload);
+    const response = await octokit.repos.compareCommits({ base, head, owner, repo });
+    
+    if (response.status !== 200) throw `The API request for this ${context.eventName} event returned ${response.status}, expected 200.`;
+    if (respose.data.status !== "ahead") throw `The head commit for this ${context.eventName} event is not ahead of the base commit.`;
+    
+    const files = response.data.files;
+    
+    // find the target file/folder
+    const file = files;
+    
+    if (!file) throw `None of the files in this commits diff tree match the provided file (${path}).`;
+    
+    core.setOutput("added", file.status === "added");
+    core.setOutput("modified", file.status === "modified");
+    core.setOutput("removed", file.status === "removed");
+    core.setOutput("renamed", file.status === "renamed");
+    core.setOutput("name", file.filename);
+    return;
+  } catch (error) {
+    core.setFailed(error);
+  }
+})();
