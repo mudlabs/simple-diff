4571d2f798fdcf4063d7b5f4c8965a688d3058d4
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-10-15 18:53:18 +1100

    Update action.js

--- action.js
+++ action.js
@@ -26,7 +26,7 @@ const getHead = name => data => name === "pull_request" ? data.pull_request.head
     const path = core.getInput("path");
     const target_name = path.split("/").pop();
     const regexp = new RegExp(target_name);
-    const file = files.some(file => regexp.test(file.filename) ? file : false);
+    const file = files.some(_file => regexp.test(_file.filename) ? _file : false);
     
     if (!file) throw `None of the files in this commits diff tree match the provided file (${path}).`;
     
