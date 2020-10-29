c07f1e3dacfdf7ab451553305bf70dd1241b1c7d
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-10-29 19:31:49 +1100

    Update action.js

--- action.js
+++ action.js
@@ -23,7 +23,7 @@ const setFromPath = octokit => owner => async repo => {
     const data = yaml.safeLoad(file);
     
     
-    console.log(workflow, file, data);
+    console.log(workflow, file, data, data.on);
     return undefined;
   } catch(error) {
     console.error(error.message);
