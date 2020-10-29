afb477d5a9e26c567d9244d41f9f26b55ba1d48e
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-10-29 18:52:58 +1100

    Update action.js

--- action.js
+++ action.js
@@ -18,7 +18,7 @@ const setFromPath = octokit => owner => async repo => {
     const workflow_id = "Test%20Diff";//process.env.GITHUB_WORKFLOW;
     const workflows = await octokit.request(
       "GET /repos/:owner/:repo/actions/workflows/:workflow_id",
-      { owner, repo, workflow_id }
+      { owner, repo, workflow_id: "test.yaml" }
     );
     
     console.log(workflow_filename, workflows, workflows.data);
